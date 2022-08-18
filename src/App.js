54e7f8abf619661465897bcb91df07cf6bd69c7c
namgon <loginonlyyy@gmail.com>
2022-08-19 00:04:09 +0900

    feat: 로고 및 문구 변경

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import logo from "./logo.svg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={logo} alt="logo" />
+        <p>짱돌디자인</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          졸업프로젝트 - 짱돌디자인
         </a>
       </header>
     </div>
